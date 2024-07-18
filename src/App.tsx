c788f4111669598206a64c68fffe92e770261a58
Stephen Vincent <stevevincent17@icloud.com>
2024-07-18 07:57:18 +0100

    practiced expandedText component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
-
-import Button from "./components/Button";
+import ExpandableText from "./components/ExpandableText";
 
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: "spicy pepperoni",
-    topping: ["Mushroom"],
-  });
-
-  const handleClick = () => {
-    setPizza({ ...pizza, topping: [...pizza.topping, "Cheese"] });
-    console.log(pizza);
-  };
-
   return (
     <div>
-      <Button onClick={handleClick}>My Button</Button>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        perspiciatis non cumque nostrum accusamus doloribus veritatis.
+        Laudantium vel ut, nulla laborum impedit alias corporis adipisci cum
+        cupiditate ipsum perferendis ea mollitia fuga. Aliquid voluptatem quae
+        facere iste aperiam ea odio, blanditiis error commodi animi est culpa
+        ratione dicta quo itaque quidem illo deleniti fugit vel reiciendis!
+        Culpa possimus laudantium nostrum maiores quas incidunt saepe iure
+        cupiditate temporibus nihil ipsam rerum voluptatibus ipsa hic veniam
+        magni harum in porro beatae, necessitatibus provident minus unde!
+        Cupiditate, corporis iusto reprehenderit vero dolorum, assumenda
+        deleniti quidem fugit dignissimos nihil sed unde impedit ducimus
+        repellat.
+      </ExpandableText>
     </div>
   );
 };

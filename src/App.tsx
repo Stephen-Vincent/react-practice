662e9d95a8ba080d81245db8e5fee80d5074148b
Stephen Vincent <stevevincent17@icloud.com>
2024-07-18 08:31:49 +0100

    using the useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-        perspiciatis non cumque nostrum accusamus doloribus veritatis.
-        Laudantium vel ut, nulla laborum impedit alias corporis adipisci cum
-        cupiditate ipsum perferendis ea mollitia fuga. Aliquid voluptatem quae
-        facere iste aperiam ea odio, blanditiis error commodi animi est culpa
-        ratione dicta quo itaque quidem illo deleniti fugit vel reiciendis!
-        Culpa possimus laudantium nostrum maiores quas incidunt saepe iure
-        cupiditate temporibus nihil ipsam rerum voluptatibus ipsa hic veniam
-        magni harum in porro beatae, necessitatibus provident minus unde!
-        Cupiditate, corporis iusto reprehenderit vero dolorum, assumenda
-        deleniti quidem fugit dignissimos nihil sed unde impedit ducimus
-        repellat.
-      </ExpandableText>
+      <Form />
     </div>
   );
 };
